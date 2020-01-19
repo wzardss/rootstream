b536e9a12a3e62ac3de7abe8a5150e81efe1aa0f
NErgezinger <noah.ergezinger@gmail.com>
2020-01-18 17:45:22 -0700

    Home links to index properly

--- rootstream/dashboard/urls.py
+++ rootstream/dashboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('signup', views.signup, name='signup'),
     path('about', views.about, name='about'),
